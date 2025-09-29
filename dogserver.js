f3dfab86c0255aea8e8d6b4e5a4a36af990cef48
MosesKollam <StephenMosesKollam@outlook.com>
2025-09-29 16:49:20 +0530

    Add files via upload

---
+++ dogserver.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const path = require('path');
+
+const base_path = `${__dirname}`;
+const index = "dogs.html"
+
+fs.readFile(path.join(base_path, index), function (error, data) {
+    if (error) {
+        console.log(error);
+    }
+    const server = http.createServer(function (request, response) {
+        const parsedUrl = url.parse(request.url, true);
+        const pathname = parsedUrl.pathname;
+        if (pathname === '/' || pathname === '/overview') {
+            response.writeHead(200, { 'Content-type': 'text/html' });
+            response.end(data);
+        } else {
+            let contentType;
+            const path_end = path.join(base_path, pathname);
+            fs.readFile(path_end, function (error, data) {
+                if (error) {
+                    return error;
+                }
+                const ext = path.extname(path_end);
+                if (ext === '.css') contentType = "text/css";
+                if (ext === '.js') contentType = "application/javascript";
+                if (ext === '.png') contentType = "image/png";
+                if (ext === '.jpg') contentType = "image/jpg";
+                if (ext === '.jpeg') contentType = "image/jpeg";
+                if (ext === '.html') contentType = "text/html";
+                response.writeHead(200, { 'Content-type': contentType });
+                response.end(data);
+            })
+        }
+    })
+    server.listen(8000, '127.0.0.1', function () {
+        console.log('Server running');
+    })
+})
